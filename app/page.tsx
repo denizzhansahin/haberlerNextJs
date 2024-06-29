491dd31cd9ae1c1995b67a122d440414dc77dc56
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2024-06-29 10:59:04 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <Link href="sayfalar/genelHaberler">
-          Hemen Haberleri Okumak için Tıklayın! WEB Sitemiz üzerinden Okumak için Tıklayın ! Tüm Dünyanın Haberi Yanında!&nbsp;
+          Hemen Haberleri Okumak için Tıklayın! WEB Sitemiz üzerinden Okumak için Tıklayın ! Tüm Dünyanın Haberi Yanında!!!!&nbsp;
 
           </Link>
         </p>
